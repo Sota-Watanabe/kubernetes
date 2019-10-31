b246772d21a8deeaee16b948a588f319882035c8
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-10-31 22:30:18 +0900

    add checkCP

--- vendor/github.com/docker/docker/client/container_start.go
+++ vendor/github.com/docker/docker/client/container_start.go
@@ -5,34 +5,31 @@ import (
 	"net/url"
 	"k8s.io/klog" //add
 	"regexp" //add
-	// "os"//add
-	// "time"//add
+	"os"//add
+	"bufio"//add
 	"github.com/otiai10/copy"
 	"github.com/docker/docker/api/types"
 )
-
+const file = "checkpoint-list.dat"
 // ContainerStart sends a request to the docker daemon to start a container.
 func (cli *Client) ContainerStart(ctx context.Context, containerID string, options types.ContainerStartOptions) error {
 	query := url.Values{}
-	// klog.V(3).Infof("so-ta: ctx=%v, containerID=%v, opt=%v", ctx, containerID, options)
 	klog.V(3).Infof("so-ta: 1 container_start.go")
-	klog.V(3).Infof("so-ta: containerID + , = %v", containerID)
-
+	// klog.V(3).Infof("so-ta: containerID + , = %v", containerID)
+	dir, err := os.Getwd()
+	klog.V(3).Infof("so-ta: dir = %v", dir)
 	req := regexp.MustCompile(`^.*,`)
 	kService := req.ReplaceAllString(containerID, "")
 	req = regexp.MustCompile(`(,.*$)`)
 	containerID = req.ReplaceAllString(containerID, "")
 	
-	if kService == "helloworld-go" {
-		klog.V(3).Infof("so-ta: checkpoint stop!")
-		// time.Sleep(30 * time.Second)
-		klog.V(3).Infof("so-ta: checkpoint start!")
-		newname := "/var/lib/docker/containers/" + containerID + "/checkpoints/cp-helloworld-go"
-		err := copy.Copy("/cp/cp-helloworld-go/", newname)
+	if checkCP(kService, file) {
+		newname := "/var/lib/docker/containers/" + containerID + "/checkpoints/" + kService
+		err := copy.Copy("/cp/" + kService, newname)
 		klog.V(3).Infof("so-ta: err=%v",err)
 		// klog.V(3).Infof("so-ta: containerID=%v", containerID)
 		// klog.V(3).Infof("so-ta: kService =%v", kService)
-		options.CheckpointID = "cp-helloworld-go"
+		options.CheckpointID = kService
 		// options.CheckpointDir = "/cp/"
 	}
 	if len(options.CheckpointID) != 0 {
@@ -48,4 +45,23 @@ func (cli *Client) ContainerStart(ctx context.Context, containerID string, optio
 	klog.V(3).Infof("so-ta: err=%v", err)
 	ensureReaderClosed(resp)
 	return err
+}
+
+func checkCP(kService string, file string) bool{
+	klog.V(3).Infof("so-ta: kService: %v", kService)
+	fp, err := os.Open(file)
+    if err != nil {
+        klog.V(3).Infof("so-ta: No such file")
+    }
+    defer fp.Close()
+
+    scanner := bufio.NewScanner(fp)
+
+    for scanner.Scan() {
+        if scanner.Text() == kService {
+			klog.V(3).Infof("so-ta: match! checkpoint: %v", kService)
+			return true
+		}
+    }
+	return false
 }
